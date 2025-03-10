e6146b40e251ee5075a0cbf55f2b121dee776b2e
Anita Norouzi <89476399+anitaNorouzi@users.noreply.github.com>
2025-03-10 13:58:13 +0330

    Initial Folders

---
+++ tailwind.config.ts
@@ -0,0 +1,33 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Shabnam"],
+    },
+    extend: {
+      zIndex: {
+        1: '1',
+        2: '2',
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        mainRed: "#f7858d",
+        mainBlue:"#009ec9",
+        mainGray:"#515151",
+        lightGray:"#676767",
+        secondLightGray:'#818181',
+        mainBlack:"#212529",
+        secondBlack:"#495057",
+
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
